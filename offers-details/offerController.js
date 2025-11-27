83cac447b93815e6c4d8a280eef167af13dfa368
André Rodrigues <oandredev@gmail.com>
2025-11-26 22:07:21 -0300

    feat: part 1 - save selection to part 2

---
+++ offers-details/offerController.js
@@ -0,0 +1,8 @@
+import { saveOfferSelection } from "../services/cartService/cartService.js";
+
+console.log("offerController loaded");
+export function trySaveOfferSelection(offerId) {
+  saveOfferSelection(offerId);
+}
+
+window.trySaveOfferSelection = trySaveOfferSelection;
